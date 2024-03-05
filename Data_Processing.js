f01e52715d85101173edd21fdc6b73f8a67d3502
Gubin Zhao <gbzhao@outlook.com>
2024-03-05 22:31:34 +0000

    完成section5

--- Data_Processing.js
+++ Data_Processing.js
@@ -1,5 +1,3 @@
-const { time } = require('console');
-
 
 class Data_Processing {
     constructor() {
@@ -14,54 +12,50 @@ class Data_Processing {
                 return;
             }
             // 这里可以处理CSV文本
-            let rows = data.split('\n');
-            for (let i = 1; i < rows.length; i++) {
-                let row = rows[i].split(',');
-                if (row.length === 1) {
-                    continue;
-                }
-                this.raw_user_data.push(row);
-            }
+            this.raw_user_data = data;
         });
     };
 
     format_Data() {
-        let formatted_data = [];
-        for (let i = 0; i < this.raw_user_data.length; i++) {
-            let user = this.raw_user_data[i];
+        var formatted_data = [];
+        let lines = this.raw_user_data.split('\r\n');
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i].split(',');
             let formatted_user = {};
-            let user_data = user[0].split(" ");
-            if (["Mr", "Mrs", "Miss", "Ms", "Dr"].includes(user_data[0])) {
-                formatted_user.Title = user_data[0];
-                user_data.shift();
+            let user_name = line[0].split(' ');
+            if (["Mr", "Mrs", "Miss", "Ms", "Dr"].includes(user_name[0])) {
+                formatted_user.title = user_name[0];
+                user_name.shift();
             }
-            formatted_user.First_Name = user_data[0];
-            user_data.shift();
-            if (user_data.length > 1) {
-                formatted_user.Middle_Name = user_data[0];
-                user_data.shift();
+            formatted_user.first_name = user_name[0];
+            user_name.shift();
+            if (user_name.length > 1) {
+                formatted_user.middle_name = user_name[0];
+                user_name.shift();
             }
             else {
-                formatted_user.Middle_Name = "";
+                formatted_user.middle_name = '';
             }
-            formatted_user.Surname = user_data[0];
+            formatted_user.surname = user_name[0];
 
-            formatted_user.Date_of_birth = user[1];
+            formatted_user.date_of_birth = line[1];
 
-            formatted_user.Age = user[2];
+            formatted_user.age = line[2];
 
-            formatted_user.Email = user[3];
+            formatted_user.email = line[3];
 
             formatted_data.push(formatted_user);
-
         }
-        return formatted_data;
+        return JSON.stringify(formatted_data);
     }
 };
+
+
 
 const dataProcessor = new Data_Processing();
 dataProcessor.load_CSV('Raw_User_Data.csv');
 setTimeout(() => {
+    console.log(dataProcessor.raw_user_data);
     const formattedData = dataProcessor.format_Data();
     console.log(formattedData);
 
